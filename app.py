2f01e2298856e1b6cda155d421c74c632b10c636
Rui Coelho <ruicoelho@ua.pt>
2019-10-27 01:13:14 +0100

    User friendly interface

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-# coding=utf-8
 from flask import Flask, render_template, request, send_file
 from flask_qrcode import QRcode
 import home
 
 app = Flask(__name__)
 qrcode = QRcode(app)
+paths = home.Home.home(app)
 
 @app.route("/")
 @app.route('/home')
 def index():
-    paths = home.Home.home()
-    return render_template("qrcode.html", paths=paths)
+    return render_template("home.html", paths=paths)
 
+@app.route('/qrcode')
+def qrcodeGenerator():
+    return render_template("qrcode.html")
 
-@app.route("/qrcode", methods=["GET"])
+@app.route('/qrcodeLogo')
+def qrcodeLogo():
+    return render_template("qrcodeLogo.html")
+
+@app.route("/genqrcode", methods=["GET"])
 def get_qrcode():
     data = request.args.get("data", "")
     return send_file(qrcode(data, mode="raw", box_size=100, error_correction='H'), mimetype="image/png")
 
-@app.route("/qrcodeLogo", methods=["GET"])
+@app.route("/genqrcodeLogo", methods=["GET"])
 def get_qrcodeImage():
     data = request.args.get("data", "")
     image = request.args.get("image", "")
--- home.py
+++ home.py
@@ -1,17 +1,17 @@
 class Home():
-    def home():
+    def home(self):
         paths = [
             {
                 "titulo": "Simple QR Code",
                 "definicao": "Generate a simple QRCode in png",
-                "link": "/qrcode?data=<data>",
+                "link": "/qrcode",
                 "argumento": "Data: QR Code information"
             },
             {
                 "titulo": "Logo QR Code",
                 "definicao": "Generate a QRCode with logo in png",
-                "link": "/qrcodeLogo?data=<data>&image=<image>",
-                "argumento": "Data: QR Code Information; Image: Image to add to QR Code"
+                "link": "/qrcodeLogo",
+                "argumento": "None"
             }
         ]
         return paths
